014be0c2a73a84e21a71feea84d4b07e94f0078c
Dggz <cristianbaciu95@gmail.com>
2019-01-27 02:14:08 +0200

    removed useless setters/getters

--- lib/reminder.dart
+++ lib/reminder.dart
@@ -1,15 +1,7 @@
 class Reminder {
   final int id;
   String title;
-  String get get_title => title;
-  set set_title(String new_title){
-    title = new_title;
-  }
   String text;
-  String get get_text => text;
-  set set_text(String new_text){
-    text = new_text;
-  }
 
   Reminder({this.id, this.title, this.text});
 
